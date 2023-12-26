9fcefa2ce8805665cd1f90aae6b8dc217dccd20f
sabinecamhi <60527952+sabinecamhi@users.noreply.github.com>
2023-12-26 11:31:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+new Glide('.glide').mount();
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize Glide
+    var glide = new Glide('.my-glide', {
+      // your existing Glide configuration options
+    });
+  
+    // Set the interval for automatic slide change (4000 milliseconds = 4 seconds)
+    setInterval(function () {
+      glide.go('>');
+    }, 4000);
+  
+    // ... your existing Glide initialization code
+    glide.mount();
+  });
---
+++ js/menu.js
@@ -0,0 +1,57 @@
+
+const menuToggleLink = document.querySelector('.new-menu-toggle');
+
+menuToggleLink.addEventListener('mouseover', function() {
+  changeText(menuToggleLink, true);
+  updateText(menuToggleLink, true);
+});
+
+menuToggleLink.addEventListener('mouseout', function() {
+  changeText(menuToggleLink, false);
+  updateText(menuToggleLink, false);
+});
+
+
+
+function changeText(element, isHovered) {
+    const menuText = element.querySelector('.menu-text');
+  
+    if (isHovered) {
+      // Change text to "Open" when hovering
+      menuText.textContent = "Open";
+    } else {
+      // Revert to "Menu" when not hovering
+      menuText.textContent = "Menu";
+    }
+  }
+  
+  function change(x) {
+    x.classList.toggle("change");
+  }
+
+
+
+  function updateText(element, isHovered) {
+    const menuText = element.querySelector('.menu-text');
+    
+    if (isHovered) {
+      // Change text to "Close" when hovering
+      menuText.textContent = "Close";
+    } else {
+      // Revert to "Menu" when not hovering
+      menuText.textContent = "Menu";
+    }
+  }
+
+
+function makeCall(phoneNumber) {
+    window.location.href = 'tel:' + phoneNumber;
+}
+
+function openEmail(email) {
+    window.location.href = 'mailto:' + email;
+}
+
+function changeCursor(element) {
+    element.style.cursor = 'pointer';
+}
