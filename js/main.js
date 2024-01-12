e453e1df832fbab4abeadf3603dff94969d978d2
sabinecamhi <60527952+sabinecamhi@users.noreply.github.com>
2024-01-12 12:09:17 +0200

    main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Set the interval for automatic slide change (4000 milliseconds = 4 seconds)
     setInterval(function () {
       glide.go('>');
-    }, 2500);
+    }, 3000);
   
     // ... your existing Glide initialization code
     glide.mount();
